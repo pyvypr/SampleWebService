ee9fc5a5fb2195012018188e0594ef96ed70ceaa
Joshua Dawes <joshua.dawes@cern.ch>
2020-01-30 11:52:57 +0100

    Modify for VyPR changes.

---
+++ VyPR_queries.py
@@ -0,0 +1,13 @@
+verification_conf = {
+  "app.routes" : {
+    "paths_branching_test" : [
+      Forall(
+        t = calls('f')
+      ).Check(
+        lambda t : (
+          t.duration()._in([0, 1])
+        )
+      )
+    ]
+  }
+}
--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from VyPR.init_verification import Verification
-verification = Verification(app)
+from VyPR import Monitor
+vypr = Monitor()
+vypr.initialise(app)
 
 from app import routes
